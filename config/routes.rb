32323bd0ed0c50d838ab72403cf60f0e96273263
CL75 <funnotfame@gmail.com>
2013-06-01 09:17:27 -0700

    create angular.js directories in assets/javascripts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 VideoFeed::Application.routes.draw do
-  get "home/index"
   scope :api do
     get "/clips(.:format)" => "clips#index"
     get "/clips/:id(.:format)" => "clips#show"
   end
+  root to: "home#index"
 end
